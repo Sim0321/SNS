f8141948bf85524d2424acf0819e90703329bf54
Sim <0doogadooga@gmail.com>
2023-12-25 18:33:28 +0900

    임시

--- src/app/(beforeLogin)/layout.tsx
+++ src/app/(beforeLogin)/layout.tsx
@@ -1,4 +1,5 @@
 import { ReactNode } from "react";
+import styles from "@/app/page.module.css";
 
 type Props = {
   children: ReactNode;
@@ -7,8 +8,8 @@ type Props = {
 
 export default function Layout({ children, modal }: Props) {
   return (
-    <div>
-      비포 로그인 레이아웃
+    <div className={styles.container}>
+      {/* @modal(패러랠 라우터)를 사용하기 위해선 layout이 동일 경로에 있어야 해서 layout을 또 하나 만듦. */}
       {children}
       {modal}
     </div>
--- src/app/(beforeLogin)/login/page.tsx
+++ src/app/(beforeLogin)/login/page.tsx
@@ -2,4 +2,5 @@ import { redirect } from "next/navigation";
 
 export default function Login() {
   redirect("/i/flow/login");
+  return <div>모달떠요</div>;
 }
